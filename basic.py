8e4e27ddc7a9d7c70c5d7bc7e5fceaf7888e513b
suzinicco <lgc@aprogen.com>
2017-06-28 16:28:44 +0900

    2017-06-28

--- basic.py
+++ basic.py
@@ -1,3 +1,8 @@
-print '{0:.3f}'.format(1.0/3)
-print '{0:_^11}'.format('hello')
-print '{name} wrote {book}'.format(name='lee',book='A Byte of Python')
+# print '{0:.3f}'.format(1.0/3)
+# print '{0:_^11}'.format('hello')
+# # print '{name} wrote {book}'.format(name='lee',book='A Byte of Python')
+# 
+print 'This is the first line\nThin is the second line'
+print 'This is the first line\tThis is the second line'
+
+print 'R"Newlines are indicated by \n"'
